fd8073d66634b9b7ec36b519fd9921f3926bbb7f
shrutim <shrutivmore156@gmail.com>
2026-04-04 11:26:07 +0530

    Used Xgboost for the model and predicted output in rupees

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import joblib
+import pandas as pd
+import numpy as np
 
 app=Flask(__name__)
 
@@ -13,7 +15,26 @@ def home():
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    return "prediction logic goes here!"
+    data = {
+        'model': request.form.get('model'),
+        'vehicle_age': int(request.form.get('vehicle_age')),
+        'km_driven': float(request.form.get('km_driven')),
+        'mileage': float(request.form.get('mileage')),
+        'engine': float(request.form.get('engine')),
+        'max_power': float(request.form.get('max_power')),
+        'seats': int(request.form.get('seats')),
+        'fuel_type': request.form.get('fuel_type'),
+        'transmission_type': request.form.get('transmission_type'),
+        'seller_type': request.form.get('seller_type')
+        }
+    
+    input_df = pd.DataFrame([data]) 
+
+    prediction = model.predict(input_df)
+
+    actual_price = np.exp(prediction[0])  # reverses log transform
+
+    return render_template("index.html", prediction_text=f"Predicted Price: ₹{actual_price:,.0f}")
 
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
